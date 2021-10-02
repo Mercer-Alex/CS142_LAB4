1fd37ae7433578003e447fc2129159ea4691d100
Alex Mercer <a.mercer118@gmail.com>
2021-10-02 11:33:55 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/MultipathAdventure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultipathAdventure.dir/main.cpp.obj"
+  "MultipathAdventure.exe"
+  "MultipathAdventure.exe.manifest"
+  "MultipathAdventure.lib"
+  "MultipathAdventure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MultipathAdventure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
